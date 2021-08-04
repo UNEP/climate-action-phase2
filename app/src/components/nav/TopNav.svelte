1ba49169f951360fff369606c3e61210bca6db2e
xaquingv <xocasgv@gmail.com>
2021-08-04 11:52:36 +0200

    ADD TopNav and new logos

---
+++ app/src/components/nav/TopNav.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Logo from "./Logo.svelte";
+</script>
+<nav class="top">
+    <Logo color="blue" />
+</nav>
+
+<style>
+    .top { margin-top: 2rem;}
+</style>
--- app/src/components/text/Intro.svelte
+++ app/src/components/text/Intro.svelte
@@ -1,14 +1,67 @@
 <script>
 export let text;
 export let head;
+export let breadcrumbs;
 </script>
-<section class='col-text'>
+<section class='intro col-text'>
+  <nav role="navigation" labelledby="breadcrumbs-label">
+    <h3 id="breadcrumbs-label" class="hidden">Breadcrumbs</h3>
+    <ul class='breadcrumbs'>
+      {#each breadcrumbs as b}
+        {#if b.a}
+        <li><a href='{b.a}'>{@html b.label}</a></li>
+        {:else}
+        <li>{@html b.label}</li>
+        {/if}
+      {/each}
+    </ul>
+  </nav>
+
   {#if head}
   <h1>{@html head}</h1>
   {/if}
+
   {#if text}
-  {#each text as p,i}
+  {#each text as p}
       <p>{@html p.p}</p>
   {/each}
   {/if}
+
 </section>
+
+<style>
+  .intro { 
+    border-bottom: 1px solid #3B536527;
+    padding-top:2rem;
+  }
+  .hidden {
+    position: absolute!important;
+    overflow: hidden;
+    clip: rect(1px,1px,1px,1px);
+    width: 1px;
+    height: 1px;
+    word-wrap: normal;
+  }
+  .breadcrumbs { 
+    list-style-type: none;
+    margin:0;
+    padding: 0;
+  }
+  li { 
+    display: inline-block;
+    margin-right:.8rem;
+    font-size: 1rem;
+    color:#333;
+    font-weight: 400;
+  }
+  li:not(:last-child)::after {
+    content: '/';
+    margin-left:.4rem;
+  }
+  li a { 
+    border: none;
+    transition: opacity .3s;
+    opacity: .68;
+  }
+  li a:hover { opacity:1; }
+</style>
