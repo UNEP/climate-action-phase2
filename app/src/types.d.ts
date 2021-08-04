fb8431c385c9d5d8953d8d68e74a705e730b1b0b
Matt Osborn <mattjosborn@gmail.com>
2021-08-04 11:31:40 +0100

    update gdocs data

--- app/src/types.d.ts
+++ app/src/types.d.ts
@@ -9,16 +9,12 @@ export interface Meta {
   description: string;
 }
 
-type ContentType = "visual" | "intro";
-type VisualType = "carto-world" | "carto-region";
-
 type TextBlockType = "p";
 
 export type TextBlock = Record<TextBlockType, string>;
 
 export interface Content {
-  type: ContentType;
-  visual: VisualType;
+  type: string;
   head: string;
   text: TextBlock[];
 }
