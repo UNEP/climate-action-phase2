c2949ab5aa5453110e05536476d0f9b346984a6b
Matt Osborn <mattjosborn@gmailcom>
2021-08-05 16:57:39 +0100

    cartoworld stub

--- app/src/components/CartoWorld.svelte
+++ app/src/components/CartoWorld.svelte
@@ -1,1 +1,42 @@
-<div>carto-world placeholder</div>
+<script lang="ts">
+  import Cartogram, { CountryDataPoint } from "src/components/maps/Cartogram.svelte";
+  import pm25data from 'src/data/pm25_coords.json';
+
+  export var data: string; // "pm25" / "health"
+
+  let selectedDataset = pm25data;
+  let selectedNodeSize = 125;
+
+  let cartogramData: CountryDataPoint[] = selectedDataset
+    .map(d => {
+      // d =
+      // {
+      //   "id": "AFG",
+      //   "pm25": 44.6,
+      //   "x": 481.366,
+      //   "y": 135.288
+      // }
+
+      return {
+        name: 'XXX',
+        short: 'XXX',
+      }
+    });
+
+</script>
+
+<div class="container">
+  <Cartogram
+    data={cartogramData}
+    domain={[700, 400]}
+    categoryFn={() => null}
+    hoverTextFn={() => 'hover text'}
+    nodeSize={selectedNodeSize}
+  />
+</div>
+
+<style>
+  .container {
+
+  }
+</style>
