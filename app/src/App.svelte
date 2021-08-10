c703b8b501bc58c47bfa40c07642b4da087269f4
Matt Osborn <mattjosborn@gmail.com>
2021-08-10 15:37:29 +0100

    d3 scale typings

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,13 +1,13 @@
 <script lang="ts" context="module">
 
-    export const colorSectors = scaleOrdinal()
+    export const colorSectors = scaleOrdinal<string>()
   	.domain(['intlshipping','transport','residential','commercial','industry','afciddust',
             'othercombustion','remainingsources','otherfires','agrwasteburning','agriculture',
             'windblowndust','waste','solvents','energy'])
   	.range(['#9b7ccc','#811494','#407aa9','#ff9c9c','#ab4867','#b3b3b3','#8c8c8c','#666666',
             '#333333','#62b048','#1b6e29','#faba26','#bd8e71','#854f38','#ff8a18']);
 
-    export const colorFuels = scaleOrdinal()
+    export const colorFuels = scaleOrdinal<string>()
   	.domain(['liquid','process','solidbio','coal'])
   	.range(['#faba26','#407aa9','#62b048','#333333']);
 
