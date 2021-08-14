7c28b58e4c0124f83318c2aebdff6043e5fb430e
MKenz0 <miguel7vj@gmail.com>
2021-08-14 17:10:20 +0200

    Distribution Viz component created

--- app/src/components/HorBarComponent.svelte
+++ app/src/components/HorBarComponent.svelte
@@ -1,22 +1,43 @@
 <script lang="ts" context="module">
-
+    export interface CountryDataSquare {
+        id: string,
+        value: number
+    }
 </script>
 
 <script lang="ts">
     import Typeahead from "svelte-typeahead";
     import countries from 'src/data/countryDictionary.json';
     import BarChart from 'src/components/charts/BarChart.svelte';
+    import ColoredSquareDistribution from 'src/components/charts/coloredSquareDistribution.svelte';
     import fuels from 'src/data/fuels.json';
     import sectors from 'src/data/sectors.json';
     import pm25data from 'src/data/pm25.json';
     import healthData from 'src/data/health.json';
+
+    let countryPM25Data: CountryDataSquare[] = pm25data
+    .map(d => {
+        return {
+            id: d.id,
+            value: d.pm25
+        };
+    });
+
+    let countryHealthData: CountryDataSquare[] = healthData
+    .map(d => {
+        return {
+            id: d.id,
+            value: d.rate
+        }
+    })
 
     const extract = (item) => item.name;
     const head = `Lorem <b>ipsum dolor sit amet</b>, consectetur adipiscing elit. Mauris mattis posuere faucibus.`;
 
     let events = [];
 
-    let currentCountry = {
+    $: currentCountry = {
+        id: "USA",
         PM25country: 0,
         timesPM25: 0,
         totalDeaths: 0,
@@ -27,6 +48,7 @@
         events = [...events, { event, detail }];
         if (event === "select"){
             let newID = detail.original.id;
+            currentCountry.id = newID;
             console.log(detail.original.id);
             currentCountry.PM25country = pm25data.find(c => c.id === newID).pm25;
             currentCountry.timesPM25 = parseFloat((currentCountry.PM25country/10).toFixed(1));
@@ -60,12 +82,32 @@
     <div class="summary">
         <div class="narrow">
             <p><span class="bigger-text">{currentCountry.PM25country}</span>{@html PM25commentary}</p>
+
+            <div>
+                <ColoredSquareDistribution
+                    data = {countryPM25Data}
+                    selectedCountry = {currentCountry.id}
+                    selectedDataset = "pm25"
+                />
+            </div>
+
         </div>
 
         <div class="right-narrow">
             <p><span class="bigger-text">{currentCountry.deathRatio}</span>{@html PMtimesCommentary}</p>
+
+            <div>
+                <ColoredSquareDistribution
+                    data = {countryHealthData}
+                    selectedCountry = {currentCountry.id}
+                    selectedDataset = "health"
+                />
+            </div>
+
         </div>
     </div>
+
+
 
 </section>
 
@@ -80,7 +122,7 @@
     }
     :global([data-svelte-typeahead]) {
         margin: 0rem;
-        width: 350px;
+        width: 450px;
         margin-top: 30px;
         z-index: 1000;
     }
@@ -91,19 +133,19 @@
     }
 
     :global([data-svelte-search] input) {
-    width: 100%;
-    padding: 0.5rem 0.75rem;
-    background: #f9f9f9;
-    font-size: 1rem;
-    border: 0;
-    border-radius: 0;
-    border: 0px solid #e5e5e5;
-    border-bottom-width: 2px;
+        width: 100%;
+        padding: 0.5rem 0.75rem;
+        background: #f9f9f9;
+        font-size: 1rem;
+        border: 0;
+        border-radius: 0;
+        border: 0px solid #e5e5e5;
+        border-bottom-width: 2px;
     }
 
     :global([data-svelte-search] label) {
-    margin-bottom: 0.25rem;
-    display: inline-flex;
-    font-size: 0.875rem;
+        margin-bottom: 0.25rem;
+        display: inline-flex;
+        font-size: 0.875rem;
     }
 </style>
---
+++ app/src/components/charts/coloredSquareDistribution.svelte
@@ -0,0 +1,99 @@
+<script lang="ts" context="module">
+    import { colorPM25, colorHealth } from "src/App.svelte";
+</script>
+
+<script lang="ts">
+import { select } from "d3-selection";
+import type {CountryDataSquare} from 'src/components/HorBarComponent.svelte';
+
+    const width = 450;
+    const height = 100;
+    const normalTileWidth = 3;
+    const normalTileHeight = 20;
+    const relevantTileWidth = 5;
+    const relevantTileHeight = 34;
+
+    export var selectedCountry: string;
+    export var data: CountryDataSquare[];
+    export var selectedDataset: string;
+
+    const colorFunction = (d: number) => selectedDataset === "pm25" ? colorPM25(d) : colorHealth(d);
+
+    function findMaxValue(data: CountryDataSquare[]){
+        let max = 0;
+        data.forEach(c => {
+            if (c.value > max){
+                max = c.value;
+            }
+        });
+        return max;
+    }
+
+    function findXlocation(countryValue: number){
+        return countryValue * (width-4) / maxValue;
+    }
+
+    let maxValue = findMaxValue(data);
+
+    $: relevantCountry = {
+        id: selectedCountry,
+        value: data.find(c=>c.id === selectedCountry).value,
+        relevantCountryColor: colorFunction(data.find(c=>c.id === selectedCountry).value)
+    }
+
+</script>
+
+<div>
+    <svg id="barchart" {width} {height}>
+        {#each data as d}
+            {#if d.value != null && d.id != selectedCountry}
+                <g id="ID" class="region">
+                    <rect
+                        id = {d.id}
+                        class= "normalTile"
+                        width= {d.id === selectedCountry ? relevantTileWidth : normalTileWidth}
+                        height= {d.id === selectedCountry ? relevantTileHeight : normalTileHeight}
+                        x={findXlocation(d.value)}
+                        y=10
+                        rx="10"
+                        ry="3"
+                        filter="none"
+                        style = "--theme-color: {colorFunction(d.value)}"
+                    />
+                </g>
+            {/if}
+        {/each}
+        <g id="ID" class="region2">
+            <rect
+                id = {relevantCountry.id}
+                class= "selectedCountry"
+                width= {relevantTileWidth}
+                height= {relevantTileHeight}
+                x={findXlocation(relevantCountry.value)}
+                y=4
+                rx="10"
+                ry="3"
+                filter="none"
+                style = "--theme-color: {colorFunction(relevantCountry.value)}"
+            />
+        </g>
+    </svg>
+</div>
+
+<style>
+
+    .selectedCountry {
+        fill: var(--theme-color);
+        stroke-width: 1.2;
+        stroke: black;
+    }
+
+    .normalTile {
+        fill: var(--theme-color);
+    }
+
+    .region2 {
+        filter: drop-shadow(3px 5px 2px rgb(0 0 0 / 0.6));
+    }
+
+</style>
