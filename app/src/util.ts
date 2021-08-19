232d688ab43f2da98126ba7f26b484ae3d6f6c48
Aleix <u1956244@campus.udg.edu>
2021-08-19 11:16:12 +0200

    indent

--- app/src/util.ts
+++ app/src/util.ts
@@ -86,11 +86,11 @@ export interface RGB{
     b: number
 }
 
-export function hexToRgb(hex:string) : RGB  {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    } : null;
+export function hexToRgb(hex:string) : RGB {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
 }
