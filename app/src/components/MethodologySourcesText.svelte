ed9646402b8f42d6842a0e4d5eab06ea742c5c3f
MKenz0 <miguel7vj@gmail.com>
2021-08-23 10:03:52 +0200

    Format adjusting

--- app/src/components/MethodologySourcesText.svelte
+++ app/src/components/MethodologySourcesText.svelte
@@ -4,20 +4,24 @@
     export var text: TextBlock[];
 
     var height = 500;
-    var width = 500;
+    var width = 700;
+
+    const numTexts = text.length;
 
     console.log("IM HERE");
 
 </script>
 
-<div class="infoSquare" style="width:{width}px; height:{height}px">
+<div class="infoSquare" style="width:{width}px">
     <h2 class='narrow'>{@html head}</h2>
 
     {#each text as t}
         <p class='info-text'>{@html t.p}</p>
-        <svg height="5" width="500">
-            <line x1="50" y1="0" x2="450" y2="0" style="stroke:#BEBEBE;stroke-width:2.5" />
-        </svg>
+        {#if t !== text[numTexts-1]}
+            <svg height="5" width="700">
+                <line x1="50" y1="0" x2="650" y2="0" style="stroke:#BEBEBE;stroke-width:2.5" />
+            </svg>
+        {/if}
 	{/each}
 </div>
 
@@ -25,7 +29,6 @@
  
     .infoSquare {
         padding:.8rem 2rem;
-        margin-bottom: 2rem;
         margin-top:2rem;
         background-color:#E8E8E8;
         overflow: auto;
