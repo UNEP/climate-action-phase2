25102892f841aa64852a9504323f48b8d6cae96b
Matt Osborn <mattjosborn@gmail.com>
2021-08-25 14:15:48 +0100

    fix methodology mobile styling

--- app/src/components/MethodologySourcesText.svelte
+++ app/src/components/MethodologySourcesText.svelte
@@ -9,24 +9,22 @@
 </script>
 
 <div class="infoSquare">
-  <ScrollableX>
-    <h2 class='narrow'>{@html head}</h2>
+  <h2 class='narrow'>{@html head}</h2>
 
-    {#each text as t}
-      <p class='info-text'>{@html t.p}</p>
-      {#if t !== text[numTexts - 1]}
-        <svg height="5" width={width}>
-          <line x1="50" y1="0" x2={width - 50} y2="0" style="stroke:#BEBEBE;stroke-width:2.5" />
-        </svg>
-      {/if}
-    {/each}
-  </ScrollableX>
+  {#each text as t}
+    <p class='info-text'>{@html t.p}</p>
+    {#if t !== text[numTexts - 1]}
+      <svg height="5" width={width}>
+        <line x1="50" y1="0" x2={width - 50} y2="0" style="stroke:#BEBEBE;stroke-width:2.5" />
+      </svg>
+    {/if}
+  {/each}
 </div>
 
 <style>
   .infoSquare {
-    padding:1rem 7rem;
-    margin-top:2rem;
+    margin: 0 -2rem;
+    padding: 1rem 2rem;
     background-color:#E8E8E8;
     overflow: auto;
     max-width: 800px;
@@ -39,5 +37,12 @@
   h2 {
     font-size: 1.15rem;
     font-weight: 600;
+  }
+
+  @media (min-width: 58rem) {
+    .infoSquare {
+      margin: 1rem 0 0;
+      padding: 1rem 7rem;
+    }
   }
 </style>
