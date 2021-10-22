d36062f03a6cafac88e7b383368caa5cad225e64
xaquingv <xocasgv@gmail.com>
2021-10-22 17:49:45 +0200

    UPDATE CO2 data from 1970 to 2019

--- data/src/index.ts
+++ data/src/index.ts
@@ -1,8 +1,13 @@
 import ghg from '../input/ghg.json';
 import fs from 'fs';
+import * as aq from 'arquero';
 import path from 'path';
+import _ from 'underscore';
 
 const OUTPUT_DIR = path.join(__dirname, '../output');
+
+const co2_totals = fs.readFileSync(path.resolve(__dirname, '../input/co2-totals.csv'));
+const co2_percapita = fs.readFileSync(path.resolve(__dirname, '../input/co2-percapita.csv'));
 
 const outputJSONData = (filename: string, data: any) => {
   const dataStr = JSON.stringify(data, null, 2);
@@ -10,17 +15,50 @@ const outputJSONData = (filename: string, data: any) => {
   fs.writeFileSync(outfile, dataStr);
 }
 
-const ghgMapped = ghg.map(row => ({
-  code: row.code,
-  emissions2015: row.emissions['2015']
-}))
+// const ghgMapped = ghg.map(row => ({
+//   code: row.code,
+//   emissions2015: row.emissions['2015']
+// }))
 
-const trendsMapped = ghg.map(row => ({
-  code: row.code,
-  size: 1,
-  emissions: row.emissions
-}))
+// const trendsMapped = ghg.map(row => ({
+//   code: row.code,
+//   size: 1,
+//   emissions: row.emissions
+// }));
 
+const foldData = (csv, not, as) => {
+  const data = aq.fromCSV(String(csv))
+    .fold(aq.not(not), {as:as});
 
-outputJSONData('ghg.json', ghgMapped);
-outputJSONData('trends.json', trendsMapped);
+  return data.objects();
+}
+
+const folded_co2_totals = foldData(co2_totals, ['id', 'country_name'], ['year', 'emissions']);
+const folded_co2_percapita = foldData(co2_percapita, ['id', 'country_name'], ['year', 'emissions']);
+
+const co2Mapped = folded_co2_totals
+  .filter(d => d.year === '2019')
+  .map(d => ({code: d.id, emissions2019: d.emissions}));
+
+const co2percapitaMapped = folded_co2_percapita
+  .filter(d => d.year === '2019')
+  .map(d => ({code: d.id, emissions2019: d.emissions}));
+
+const co2trendsMapped = [...new Set(folded_co2_totals.map(d => d.id))]
+  .map(d => (
+    {
+      code: d,
+      size: 1,
+      emissions: Object.fromEntries(
+        folded_co2_totals
+          .filter(_d => _d.id === d)
+          .map(d => [d.year, d.emissions])
+      )  
+    }
+  ))
+
+// outputJSONData('ghg.json', ghgMapped);
+// outputJSONData('trends.json', trendsMapped);
+outputJSONData('co2.json', co2Mapped);
+outputJSONData('co2trends.json', co2trendsMapped);
+outputJSONData('co2percapita.json', co2percapitaMapped);
