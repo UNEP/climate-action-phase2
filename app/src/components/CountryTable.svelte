6ec5c2b62639db5e039d3e1b5a357b66a2e46a8f
Matt Osborn <mattjosborn@gmail.com>
2021-10-25 20:17:07 +0100

    CountryTable refactor

--- app/src/components/CountryTable.svelte
+++ app/src/components/CountryTable.svelte
@@ -6,57 +6,35 @@
 </script>
 
 <script lang="ts">
-  import Typeahead from "svelte-typeahead";
-  import countries from 'src/data/countryDictionary.json';
-  import countryTableData2 from 'src/data/countryTableData2.json';
   import co2trends from 'src/data/co2trends.json';
   import co2data from 'src/data/co2data.json';
+  import type { Unpacked } from 'src/util';
   import { createLookup } from 'src/util';
   import MiniLineChart from "src/components/charts/MiniLineChart.svelte";
-  import DistributionTiles from "src/components/charts/DistributionTiles.svelte";
-
-  export var data : "GHG" | "Risk";
-
-  const countryTableLookUp = createLookup(co2data, d => d.id, d => d);
 
   const description = "It has had one of the biggest increases in GHG emissions\
    -422% since 1990. Today, it accounts for 0.33% of global emissions.";
 
-  const numMainCountriesTable = 6;
-  const maxNumSearchResults = 6;
-  const extract = (item) => item.name;
   const head = `Lorem <b>ipsum dolor sit amet</b>, consectetur adipiscing elit.
     Mauris mattis posuere faucibus.`;
   const emissions2015Comment = 'million tonnes of GHG';
   const globalPCTComment = `<t style="font-size:16px;">%</t>`;
   const perCapitaComment = `tonnes<br> of GHG`;
 
-  var selectedDatabase = data === "GHG" ? co2data : co2data;
+  const ROW_LIMIT = 6;
 
-  var buttonMode: "First" | "Search" | "All";
-  buttonMode = "All";
-  var buttonTextOptions = {"All": "Show all countries", "First": "Show only main"};
-  var buttonText = buttonTextOptions[buttonMode];
+  type RowData = Unpacked<typeof co2data>;
 
-  var tableMode : "First" | "Search" | "All";
-  tableMode = "First";
+  let sortedData = co2data;
 
-  var sortingMethod: 'Ascending' | 'Descending';
-  sortingMethod = 'Descending';
-
-  var currentSortingHeader = 'country';
-
-  let results = selectedDatabase;
-  showFirstTables(currentSortingHeader);
+  let showAll = false;
 
   let widthLineChart = 225;
-  let heightLineChart = 100;
-  let widthDistribution = 10;
+  // let heightLineChart = 100;
+  // let widthDistribution = 10;
 
-
-
-  var countryEmissions = [];
-  var countryDataArray = co2trends;
+  let countryEmissions = [];
+  let countryDataArray = co2trends;
 
   for (let i = 0; i < countryDataArray.length; i++){
     let emissionsArray = Object.entries(countryDataArray[i].emissions);
@@ -80,137 +58,88 @@
 
   const countryTrendLookUp = createLookup(countryEmissions, c => c.id, c => c);
 
-
   function getCategory(baseValue, lastValue) {
     let diff = (lastValue - baseValue) / baseValue;
     if (Math.abs(diff) < 0.25)
       return 'stable';
-    else if (diff < -0.25) 
+    else if (diff < -0.25)
       return 'falling';
-    else 
+    else
       return 'climbing-fast';
   }
 
+  type Header = { name: string, sortable: boolean, defaultSort?: 'asc' | 'desc' };
 
-  function howManyButtonClicked(){
-    if (buttonMode === "All"){
-      buttonMode = "First";
-      tableMode = "All";
-      buttonText = buttonTextOptions[buttonMode];
-      results = selectedDatabase;
-      sortByHeader(currentSortingHeader);
+  const headers: Header[] = [
+    { name: 'Country', sortable: true, defaultSort: 'asc' },
+    { name: 'Trend', sortable: false },
+    { name: '2019 emissions', sortable: true },
+    { name: 'As pct of global', sortable: true },
+    { name: 'Per capita', sortable: true },
+  ];
+
+  let sort: {column: string, asc: boolean} = {column: 'Country', asc: true};
+
+  const onClickHeader = (header: Header) => {
+    if (header.name === sort.column) {
+      sort.asc = !sort.asc;
+    } else {
+      sort = {
+        column: header.name,
+        asc: header.defaultSort === 'asc'
+      };
+    }
+  };
+
+  const getSortAsc = ({column}: typeof sort): RowData[] => {
+    if (column === 'Country') {
+      return [...co2data].sort((a,b) => a.name > b.name ? 1 : -1);
+    }
+    else if (column === '2019 emissions') {
+      return [...co2data].sort((a,b) => a.emissions2019 - b.emissions2019);
+    }
+    else if (column === 'As pct of global') {
+      return [...co2data].sort((a,b) => a.globalPct - b.globalPct);
+    }
+    else if (column === 'Per capita') {
+      return [...co2data].sort((a,b) => a.percapita - b.percapita);
+    }
+  };
+
+  $: {
+    sortedData = getSortAsc(sort);
+    if (!sort.asc) sortedData.reverse();
+  }
+
+  let searchText: string = '';
+
+  let searchResults: string[];
+  function getSearchResults(text: string): string[] {
+    const re = new RegExp(text, 'i');
+    return sortedData
+      .map(d => ({
+        id: d.id,
+        searchIndex: re.exec(d.name)?.index
+      }))
+      .filter(d => d.searchIndex !== undefined)
+      .sort((a, b) => a.searchIndex - b.searchIndex)
+      .map(d => d.id);
+  }
+
+  $: {
+    searchResults = getSearchResults(searchText);
+    sortedData = sortedData;
+  }
+
+  const displayRow = (row: RowData, i: number) => {
+    if (searchText === '') {
+      return showAll ? true : i < ROW_LIMIT;
     }
     else {
-      buttonMode = "All";
-      tableMode = "First";
-      buttonText = buttonTextOptions[buttonMode];
-      showFirstTables(currentSortingHeader);
+      const searchRank = searchResults.indexOf(row.id);
+      return searchRank !== -1 ? (showAll || searchRank < ROW_LIMIT) : false;
     }
-  }
-
-
-  //TOO MUCH REPETITIVE CODE
-  function sortByHeader(sortingColumn){
-    var arrayResults = results;
-    if (sortingColumn === "country"){
-      if (sortingMethod === 'Ascending'){
-        arrayResults.sort((a, b) => a.name.localeCompare(b.name));
-      }
-      else {
-        arrayResults.sort((a, b) => b.name.localeCompare(a.name));
-      }
-
-    }
-    else if (sortingColumn === "emissions2015"){
-      if (sortingMethod === 'Ascending'){
-        arrayResults.sort((a, b) => a.emissions2019 - b.emissions2019);
-      }
-      else{
-        arrayResults.sort((a, b) => b.emissions2019 - a.emissions2019);
-      }
-    }
-
-    else if (sortingColumn === "globalpct"){
-      if (sortingMethod === 'Ascending'){
-        arrayResults.sort((a, b) => a.globalPct - b.globalPct);
-      }
-      else{
-        arrayResults.sort((a, b) => b.globalPct - a.globalPct);
-      }
-    }
-
-    else if (sortingColumn === "percapita"){
-      if (sortingMethod === 'Ascending'){
-        arrayResults.sort((a, b) => a.percapita - b.percapita);
-      }
-      else{
-        arrayResults.sort((a, b) => b.percapita - a.percapita);
-      }
-    }
-    results = arrayResults;
-  }
-
-
-  function showFirstTables(sortingColumn){
-    sortByHeader(sortingColumn);
-    results = results.slice(0,numMainCountriesTable);
-  }
-
-
-  function reorder(selectedHeader){
-    if (currentSortingHeader !== selectedHeader){
-      currentSortingHeader = selectedHeader;
-      sortingMethod = 'Descending';
-    }
-    else {
-      if (sortingMethod === 'Descending'){
-        sortingMethod = 'Ascending';
-      }
-      else {
-        sortingMethod = 'Descending';
-      }
-    }
-
-    if (tableMode === "First"){
-      results = selectedDatabase;
-      showFirstTables(currentSortingHeader);
-    }
-    else {
-      sortByHeader(currentSortingHeader);
-    }
-  }
-
-
-  function clearSearchBar(){
-    buttonMode = "All";
-    buttonText = buttonTextOptions[buttonMode];
-    tableMode = "First";
-    results = selectedDatabase;
-    showFirstTables(currentSortingHeader);
-  }
-
-
-  function getResultFromSearch(result, index){
-    if (index === 0){
-      tableMode = "Search";
-      buttonMode = "Search";
-      results = [];
-      var firstResult = countryTableLookUp[result.id];
-      if (firstResult !== undefined){ //Because we dont have final database
-        results.push(firstResult);
-      }
-    }
-    else {
-      var countryResult = countryTableLookUp[result.id];
-      if (countryResult !== undefined){ //Because we dont have final database
-        results.push(countryResult);
-      }
-    }
-
-    sortByHeader(currentSortingHeader);
-  }
-
-  let rerender: () => void;
+  };
 
 </script>
 
@@ -218,117 +147,66 @@
 <h2 class='narrow'>{@html head}</h2>
 
 <div class="search-bar">
-  <Typeahead 
-    let:result
-    let:index
-    data={countries} 
-    {extract}
-    limit={maxNumSearchResults}
-    placeholder={ `Search a country`}
-    hideLabel
-    on:clear={() => clearSearchBar()}
-    >
-    {getResultFromSearch(result.original, index)}
-  </Typeahead>
+  <input bind:value={searchText} placeholder='Search a country' />
 </div>
 
 <div class="grid-ghg">
-  <div class="header" class:selected="{currentSortingHeader === 'country'}"
-      on:click={() => reorder('country')}
-      on:click={() => rerender()}>
-    <span>
-      <br>COUNTRY
-      {#if currentSortingHeader === 'country'}
-        <i class="arrow-down" class:arrow-up="{sortingMethod === 'Ascending'}"></i>
-      {/if}
-    </span>
-  </div>
 
-  <div class="header" class:selected="{currentSortingHeader === 'trend'}"
-    on:click={() => currentSortingHeader = 'trend'}>
-    <span>
-      <br>TREND
-      {#if currentSortingHeader === 'trend'}
-        <i class="arrow-down" class:arrow-up="{sortingMethod === 'Ascending'}"></i>
-      {/if}
-    </span>
-  </div>
+  {#each headers as h}
+    <div class="header" class:selected="{sort && sort.column === h.name}"
+        on:click={() => h.sortable && onClickHeader(h)}>
+      <span>
+        {h.name}
+        {#if sort && sort.column === h.name}
+          <i class="arrow-down" class:arrow-up={sort.asc}></i>
+        {/if}
+      </span>
+    </div>
+  {/each}
 
-  <div class="header" class:selected="{currentSortingHeader === 'emissions2015'}"
-    on:click={() => reorder('emissions2015')}
-    style="text-align:right;">
-    <span>
-      {#if currentSortingHeader === 'emissions2015'}
-        <i class="arrow-down" class:arrow-up="{sortingMethod === 'Ascending'}"></i>
-      {/if}
-      2019 EMISSIONS
-    </span>
-  </div>
+  {#each sortedData as row, i}
+    <div class="row" style={!displayRow(row, i) && 'display: none'}>
+      <span class="country-span">
+        <div class="country-column">
 
-  
-  <div class="header" class:selected="{currentSortingHeader === 'globalpct'}"
-    on:click={() => reorder('globalpct')}
-    style="text-align:right;">
-    <span>
-      {#if currentSortingHeader === 'globalpct'}
-        <i class="arrow-down" class:arrow-up="{sortingMethod === 'Ascending'}"></i>
-      {/if}
-      AS PCT OF GLOBAL
-    </span>
-  </div>
+          <div class="country-name">
+            {row.name}
+          </div>
 
-  <div class="header" class:selected="{currentSortingHeader === 'percapita'}"
-    on:click={() => reorder('percapita')}
-    style="text-align:right;">
-    <span>
-      {#if currentSortingHeader === 'percapita'}
-        <i class="arrow-down" class:arrow-up="{sortingMethod === 'Ascending'}"></i>
-      {/if}
-      PER<br>CAPITA
-    </span>
-  </div>
+          <div class="country-description">
+            {description}
+          </div>
 
-  {#each results as row}
-    <span class="country-span">
-      <div class="country-column">
-
-        <div class="country-name">
-          {row.name}
         </div>
+      </span>
 
-        <div class="country-description">
-          {description}
-        </div>
-
-      </div>
-    </span>
-
-    <span bind:clientWidth={widthLineChart}>
-        <MiniLineChart 
-          data={countryTrendLookUp[row.id].emissions} 
+      <span bind:clientWidth={widthLineChart}>
+        <MiniLineChart
+          data={countryTrendLookUp[row.id].emissions}
           category={countryTrendLookUp[row.id].category}
-          />
-    </span>
+        />
+      </span>
 
-    <span class="row-number">
-      {row.emissions2019}
-      <p class="number-descriptor">{emissions2015Comment}</p>
-    </span>
-    <span class="row-number">
-      {row.globalPct}{@html globalPCTComment}
-    </span>
-    <span class="row-number">
-      {row.percapita}
-      <p class="number-descriptor">{@html perCapitaComment}</p>
-    </span>
+      <span class="row-number">
+        {row.emissions2019}
+        <p class="number-descriptor">{emissions2015Comment}</p>
+      </span>
+      <span class="row-number">
+        {row.globalPct}{@html globalPCTComment}
+      </span>
+      <span class="row-number">
+        {row.percapita}
+        <p class="number-descriptor">{@html perCapitaComment}</p>
+      </span>
+    </div>
   {/each}
 </div>
 
-{#if buttonMode !== "Search"}
-  <button 
+{#if searchText === ''}
+  <button
     class="show-more-button"
-    on:click="{howManyButtonClicked}">
-    <b>{buttonText}</b>
+    on:click={() => showAll = !showAll}>
+    <b>{showAll ? 'Show only main' : 'Show all countries'}</b>
   </button>
 {/if}
 
@@ -359,6 +237,10 @@
   -webkit-transform: rotate(-135deg);
 }
 
+.row {
+  display: contents;
+}
+
 .country-span {
   width: 100%;
   display: table;
@@ -369,15 +251,15 @@
 }
 
 .country-name {
-  width: 35%; 
-  display: table-cell; 
+  width: 35%;
+  display: table-cell;
   font-size: 24px;
   font-weight: bold;
 }
 
 .country-description {
   display: table-cell;
-  font-weight: 100; 
+  font-weight: 100;
   font-size: 16px;
 }
 
