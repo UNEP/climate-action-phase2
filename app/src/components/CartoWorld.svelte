fc6a1a8c497e20b0190c9dc4cd5979f6fd46b42d
LeVarez <aleixac1997@gmail.com>
2021-11-04 11:09:01 +0100

    ndc carto text

--- app/src/components/CartoWorld.svelte
+++ app/src/components/CartoWorld.svelte
@@ -131,18 +131,7 @@
           ...datasets.cartoworld.ndc,
           NodeClass: CartogramDataPoint,
           NodeComponent: CartogramNode,
-          hoverTextFn: (c) => {
-            let current = datasets.lookups.netzero[c.id];
-            let res = "";
-
-            current.status !== "" && current.year !== null ?
-              res = datasets.lookups.countries[c.id].name +
-              " has the status: " + current.status + " by " + current.year + "." :
-              res = "No data for " + datasets.lookups.countries[c.id].name + ".";
-
-            return res;
-
-          },
+          hoverTextFn: (c) => c.data.label,
           colorFn: d => colorNDC(d.data.colorValue),
         }],
         countries: datasets.countries,
