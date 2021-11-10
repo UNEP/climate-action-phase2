083d3b8efe27e64b9a1c019657023fd3d6d9a99c
LeVarez <aleixac1997@gmail.com>
2021-11-10 12:28:23 +0100

    new branch

---
+++ app/src/components/NavigationPages.svelte
@@ -0,0 +1,164 @@
+<script lang="ts">
+  import Icon from './Icon.svelte';
+  import type {Content, Embeds} from '../types';
+
+
+
+  const urlParts = window.location.href.split('/');
+  const urlPath = urlParts.slice(0, -1).join('/');
+  const embedScript = `${urlPath}/embed.js`;
+
+  function selectAll(e: HTMLTextAreaElement) {
+    e.setSelectionRange(0, e.value.length);
+  }
+
+</script>
+
+<section class="infoSquare margin-breakout-mobile" id={id}>
+  <div class="kicker">
+    <div class="kicker-icon" style="">
+      <Icon name="embed" />
+    </div>
+    <div class="kicker-text">
+      EMBED OUR VISUALIZATIONS
+    </div>
+  </div>
+  <div class="embed-container">
+
+    <div class="group-embed">
+      <div class="embed-chart">
+        <img src="/img/embeds/.jpg" class="embed-image" alt="embed">
+        <div class="embed-background">
+          <textarea
+            class="embed-tag"
+          />
+        </div>
+      </div>
+      <p class="info-text">
+        <b>TITLE</b>
+        <br>
+        HEAAAAD
+      </p>
+    </div>
+  </div>
+
+</section>
+
+<style>
+  .embed-tag {
+    word-wrap: break-word;
+    word-break: break-all;
+    text-align: left;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    outline: none;
+    border: none;
+    background: transparent;
+    padding: 10px;
+    font-size: 16px;
+    font-family: 'Roboto', sans-serif;
+    color: black;
+    resize: none;
+  }
+
+  .group-embed {
+    width: 225px;
+    margin-right: 25px;
+    display: inline-table;
+    position:relative;
+  }
+  .embed-image {
+    position: absolute;
+    width: 100%;
+    height: 150px;
+    object-fit: contain;
+  }
+  .embed-background {
+    position: absolute;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #f3f3f3D0;
+    transition: .5s ease;
+    text-align: center;
+  }
+
+  .embed-background:hover {
+    opacity: 1;
+    transition: .5s ease;
+  }
+  .embed-container {
+    box-sizing: border-box;
+    padding-bottom: 20px;
+    flex: 0 0 100%;
+    text-align: left;
+    margin-top:30px;
+    flex-direction: row;
+  }
+  .embed-chart {
+    background: #f9f9f9;
+    filter: drop-shadow(0px 0px 10px rgba(0,0,0,0.2));
+
+    height: 150px;
+    border: 2px solid rgba(189, 189, 189, 0);
+  }
+  .embed-chart:hover {
+    filter: drop-shadow(0px 0px 10px rgba(0,0,0,0));
+    border: 2px solid rgba(189, 189, 189, 1);
+  }
+
+  .kicker-icon {
+    margin-right: 4px;
+    position: relative;
+    width: 36px;
+    height: 36px;
+    box-sizing: border-box;
+    color: black;
+  }
+  .kicker-text {
+    padding-left: 5px;
+    font-size: 15px;
+    font-weight: 400;
+    text-transform: uppercase;
+  }
+  .kicker {
+    position: relative;
+    max-width: 1100px;
+    height: 50px;
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    font-weight: 300;
+    white-space: nowrap;
+    margin-bottom: 5px;
+  }
+
+  .info-text {
+    color:#505050;
+    padding-bottom:1rem!important;
+    font-size: 1.125rem;
+    line-height:  1.5rem;
+  }
+
+  .infoSquare {
+    padding: 1rem 1rem;
+    background-color:#E8E8E8;
+    overflow: auto;
+    max-width: 800px;
+    margin-top: 20px!important;
+  }
+
+  @media (min-width: 400px) {
+    .infoSquare {
+      padding: 1rem 2rem;
+    }
+  }
+
+  @media (min-width: 58rem) {
+    .infoSquare {
+      margin: 1rem 0 0;
+      padding: 3.6rem;
+    }
+  }
+</style>
