983046d473d2ab75ee2c13cbea0d14445e21039d
Xaquín G.V <xocasgv@gmail.com>
2022-10-14 10:04:29 +0200

    UPDATE Turkey to Türkiye; UPDATE unep logo

--- app/src/components/nav/Logo.svelte
+++ app/src/components/nav/Logo.svelte
@@ -1,10 +1,10 @@
 <script>
-  import UNEPLogoBlue from 'src/assets/unep50.svg';
-  import UNEPLogoWhite from 'src/assets/unep50white.svg';
+  import UNEPLogoBlue from 'src/assets/unep.svg';
+  import UNEPLogoWhite from 'src/assets/unepwhite.svg';
 
   export let color = undefined;
 </script>
 
 <div class="logo">
-  <img src={!color || color === 'blue' ? UNEPLogoBlue : UNEPLogoWhite} alt='Logo of the United Nations Environment Programme | 50 year anniversary'>
+  <img src={!color || color === 'blue' ? UNEPLogoBlue : UNEPLogoWhite} alt='Logo of the United Nations Environment Programme'>
 </div>
